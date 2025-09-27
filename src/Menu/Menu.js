45d6dc7fad87aade737111a6e9c4e73d26ddd9f7
Forrest Stutts <fstutts@Mac.lan>
2025-09-27 12:55:38 -0400

    Brought PBudget code in

---
+++ src/Menu/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import {
+  Link
+} from "react-router-dom";
+
+function Menu() {
+  return (
+       <nav
+            role="navigation"
+            aria-label="Main menu"
+            itemScope
+            itemType="https://schema.org/SiteNavigationElement"
+        >
+            <ul>
+                <li><Link itemProp ="url" to="/">Homepage</Link></li>
+            
+                <li><Link to="/about" aria-label="Read more about the authors of this site.">About</Link></li>
+                <li><Link to="/login" aria-label="Login in with your credentials to access your account.">Login</Link></li>
+            </ul>
+        </nav>
+  );
+}
+
+export default Menu;
